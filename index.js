cb6c63da238ddfe61b796fefd40c7a2209ba7ce2
swiftaff <swiftaff@gmail.com>
2020-10-12 23:01:09 +0800

    fix chrome display bug with 100% width

--- index.js
+++ index.js
@@ -136,7 +136,8 @@ function getCodeFromIconList(icons) {
 }
 
 function getSVGHtmlFromData(d) {
-    return `<svg 
+    return `<svg
+width="100%" 
 xmlns="http://www.w3.org/2000/svg"
 xmlns:xlink="http://www.w3.org/1999/xlink"
 aria-hidden="true"
