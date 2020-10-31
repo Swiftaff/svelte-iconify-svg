b0257c018103e22a7bdd58a6a82aff75ef6f9877
swiftaff <swiftaff@gmail.com>
2020-10-31 17:18:33 +0800

    update cli with new options

--- bin/svelte-iconify-svg.js
+++ bin/svelte-iconify-svg.js
@@ -4,7 +4,7 @@ const svelteiconifysvg = require("../index.js");
 const yargs = require("yargs");
 
 const helpText = `
-1. syntax: svelte-iconify-svg -i inputDirectoryArray -o outputFilePath
+1. syntax: svelte-iconify-svg -i inputDirectoryArray -o outputFilePath -f -c
 If you don't supply the optional i or o parameters it uses these defaults...
 *FULL PATHS ONLY AT THE MOMENT*
 inputDirectoryArray = "src"
@@ -19,7 +19,7 @@ for any references to 'iconify' icons, i.e. any text in the format 'alphanumeric
 It will then save it as a .js file which exports an object of all iconify files found in your project.
 
 4. Basic usage
-a) add to the package.json, and run it as part of your dev or build step
+a) add to the package.json, and run it as part of your dev or build step, or use via the rollup-plugin https://github.com/Swiftaff/rollup-plugin-iconify-svg
 
 "scripts": {
   "svelteiconifysvg": "node svelte-iconify-svg"
@@ -33,7 +33,7 @@ import { icons } from "./icons.js";
 {@html icons["fa:random"]}
 <!-- note, so if this example.svelte file is in one of the input directories, the "fa:random" text above would have been found, and the icon auto-generated! -->
 `;
-//module.exports = function() {
+
 const argv = yargs
     .option("input", {
         alias: "i",
@@ -45,6 +45,18 @@ const argv = yargs
         description: 'filePath to save, default is "src/icons.js"',
         type: "string",
     })
+    .option("outputsvgfiles", {
+        alias: "f",
+        description: "outputs individual svg files, instead of the default single JS object of all icons SVG code",
+        type: "boolean",
+    })
+    .option("cjs", {
+        alias: "c",
+        description:
+            'outputs the JS object as commonJs "module.exports = ", instead of the default ES6 export syntax "export const icons = "',
+        type: "boolean",
+    })
+
     .help(true, helpText)
     .alias("help", "h").argv;
 
@@ -52,6 +64,9 @@ let inputDirectoryArray = argv.input || ["src"];
 
 let outputFilePath = argv.output || "src/icons.js";
 
-let thePath = process.argv[1];
+let options = {
+    outputSVGfiles: typeof argv.outputsvgfiles !== "undefined",
+    commonJs: typeof argv.cjs !== "undefined",
+};
 
-svelteiconifysvg(inputDirectoryArray, outputFilePath);
+svelteiconifysvg(inputDirectoryArray, outputFilePath, options);
