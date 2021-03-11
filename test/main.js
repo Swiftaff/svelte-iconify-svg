c6dea326e7a944a4db482275a57b6204921963de
swiftaff <swiftaff@gmail.com>
2021-03-11 22:36:05 +0800

    fix for test10b - circleci revert path

--- test/main.js
+++ test/main.js
@@ -59,7 +59,7 @@ test("test 10a fn - intitial missing icon file should continue and save, not err
 });
 
 test("test 10b fn - if icon file exists but is not a module or object", async (t) => {
-    const filepath = path.join(__dirname, "/outputs/test10/icons10b.js");
+    const filepath = "/test/outputs/test10/icons10b.js";
     await svelteiconifysvg(["test/fixtures/test1"], filepath, {
         commonJs: true,
     });
