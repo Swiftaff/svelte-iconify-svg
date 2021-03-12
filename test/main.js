2f65fa5d2354d9eeb38044c921085a9adb2439dc
swiftaff <swiftaff@gmail.com>
2021-03-12 22:02:38 +0800

    rename tests and outputs for consistency

--- test/main.js
+++ test/main.js
@@ -5,31 +5,31 @@ const withPage = require("./_withPage");
 const svelteiconifysvg = require("../index");
 const spawnSync = require("child_process").spawnSync;
 
-test("test1 fn - basic working", async (t) => {
+test("test 1 fn - basic working", async (t) => {
     await svelteiconifysvg(["test/fixtures/test1"], "test/outputs/test1/icons.js", { commonJs: true });
     const test1 = require("../test/outputs/test1/icons.js");
     t.snapshot(test1);
 });
 
-test("test1b fn - basic working with no options (without CommonJs)", async (t) => {
+test("test 1b fn - basic working with no options (without CommonJs)", async (t) => {
     await svelteiconifysvg(["test/fixtures/test1"], "test/outputs/test1/icons2.js");
     const test1b = await import("../test/outputs/test1/icons2.js");
     t.snapshot(test1b);
 });
 
-test("test2 fn - basic wrong input directory returns empty object", async (t) => {
+test("test 2 fn - basic wrong input directory returns empty object", async (t) => {
     await svelteiconifysvg(["test/fixtures/test22"], "test/outputs/test2/icons.js", { commonJs: true });
     const test2 = require("../test/outputs/test2/icons.js");
     t.snapshot(test2);
 });
 
-test("test3 fn - basic input string, not array", async (t) => {
+test("test 3 fn - basic input string, not array", async (t) => {
     await svelteiconifysvg("test/fixtures/test1", "test/outputs/test3/icons.js", { commonJs: true });
     const test3 = require("../test/outputs/test3/icons.js");
     t.snapshot(test3);
 });
 
-test("test4 fn - multiple inputs", async (t) => {
+test("test 4 fn - multiple inputs", async (t) => {
     await svelteiconifysvg(
         ["test/fixtures/test1", "test/fixtures/test4", "test/fixtures/test4/test4b"],
         "test/outputs/test4/icons.js",
@@ -41,7 +41,7 @@ test("test4 fn - multiple inputs", async (t) => {
     t.snapshot(test4);
 });
 
-test("test9 fn - typo in icon names should log but not fail", async (t) => {
+test("test 9 fn - typo in icon names should log but not fail", async (t) => {
     await svelteiconifysvg(["test/fixtures/test9"], "test/outputs/test9/icons.js", {
         commonJs: true,
     });
@@ -119,60 +119,65 @@ test("test 10e fn - if icon list is same as saved - DO save if alwaysSave is tru
 //test original deep directories node bin/svelte-iconify-svg -i 'test' 'test2' -o 'test/test2/test3/icons.js'
 //test CLI not just fn
 
-test("test5 cli - basic working - no options, all defaults", async (t) => {
+
+//
+// test CLI not just fn
+//
+
+test("test 5 cli - basic working - no options, all defaults", async (t) => {
     spawnSync("node", ["bin/svelte-iconify-svg"]);
     const test5 = require("../src/icons.js");
     t.snapshot(test5);
 });
 
-test("test6 cli - basic working - single input, single output", async (t) => {
+test("test 6 cli - basic working - single input, single output", async (t) => {
     const result = spawnSync("node", [
         "bin/svelte-iconify-svg",
         "-i",
         "'test/fixtures/test1'",
         "-o",
-        "'test/outputs/test6/icons.js'",
+        "'test/outputs/test6/icons_cli.js'",
     ]);
-    const test6 = require("../test/outputs/test6/icons.js");
+    const test6 = require("../test/outputs/test6/icons_cli.js");
     t.snapshot(test6);
 });
 
-test("test7 cli - basic working - multiple input", async (t) => {
+test("test 7 cli - basic working - multiple input", async (t) => {
     const result = spawnSync("node", [
         "bin/svelte-iconify-svg",
         "-i",
         "'test/fixtures/test1'",
         "'test/fixtures/test4'",
         "-o",
-        "'test/outputs/test7/icons.js'",
+        "'test/outputs/test7/icons_cli.js'",
     ]);
-    const test7 = require("../test/outputs/test7/icons.js");
+    const test7 = require("../test/outputs/test7/icons_cli.js");
     t.snapshot(test7);
 });
 
-test("test8 cli - basic working - single input, single output, commonJs", async (t) => {
+test("test 8 cli - basic working - single input, single output, commonJs", async (t) => {
     const result = spawnSync("node", [
         "bin/svelte-iconify-svg",
         "-i",
         "'test/fixtures/test1'",
         "-o",
-        "'test/outputs/test8/icons.js'",
+        "'test/outputs/test8/icons_cli.js'",
         "-cjs",
     ]);
-    const test8 = require("../test/outputs/test8/icons.js");
+    const test8 = require("../test/outputs/test8/icons_cli.js");
     t.snapshot(test8);
 });
 
-test("test9 cli - typo in icon names should log but not fail", async (t) => {
+test("test 9 cli - typo in icon names should log but not fail", async (t) => {
     const result = spawnSync("node", [
         "bin/svelte-iconify-svg",
         "-i",
         "'test/fixtures/test9'",
         "-o",
-        "'test/outputs/test9/icons.js'",
+        "'test/outputs/test9/icons_cli.js'",
         "-cjs",
     ]);
-    const test9cli = require("../test/outputs/test9/icons.js");
+    const test9cli = require("../test/outputs/test9/icons_cli.js");
     t.snapshot(test9cli);
 });
 
@@ -182,15 +187,15 @@ test("test 10a cli - intitial missing icon file should continue and save, not er
         "-i",
         "'test/fixtures/test1'",
         "-o",
-        "'test/outputs/test10/icons10a.js'",
+        "'test/outputs/test10/icons10a_cli.js'",
         "-cjs",
     ]);
-    const test10acli = require("../test/outputs/test10/icons10a.js");
+    const test10acli = require("../test/outputs/test10/icons10a_cli.js");
     t.snapshot(test10acli);
 });
 
 test("test 10b cli - if icon file exists but is not a module or object", async (t) => {
-    const filepath = "test/outputs/test10/icons10b.js";
+    const filepath = "test/outputs/test10/icons10b_cli.js";
     fs.writeFileSync(filepath, "//not a module", "utf8");
     const result = spawnSync("node", [
         "bin/svelte-iconify-svg",
@@ -201,12 +206,12 @@ test("test 10b cli - if icon file exists but is not a module or object", async (
         "-cjs",
     ]);
 
-    const test10bcli = require("../test/outputs/test10/icons10b.js");
+    const test10bcli = require("../test/outputs/test10/icons10b_cli.js");
     t.snapshot(test10bcli);
 });
 
 test("test 10c cli - if icon list is same as saved don't resave it if alwaysSave is false or missing", async (t) => {
-    const filepath = "./test/outputs/test10/icons10c.js";
+    const filepath = "./test/outputs/test10/icons10c_cli.js";
     const result1 = spawnSync("node", [
         "bin/svelte-iconify-svg",
         "-i",
@@ -233,7 +238,7 @@ test("test 10c cli - if icon list is same as saved don't resave it if alwaysSave
 });
 
 test("test 10e cli - if icon list is same as saved - DO save if alwaysSave is set", async (t) => {
-    const filepath = "test/outputs/test10/icons10e.js";
+    const filepath = "test/outputs/test10/icons10e_cli.js";
     const result1 = spawnSync("node", [
         "bin/svelte-iconify-svg",
         "-i",
